c54ce1c4d72a2bed49c8da3a171ab03cf193d6d3
anmut12 <anmut121921@gmail.com>
2025-04-04 14:20:30 +0300

    text style
    text style

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const card = document.getElementById('card');
-    const toBack = document.getElementById('toBack');
-    const toFront = document.getElementById('toFront');
-    const flipSound = document.getElementById('flipSound');
+  const card = document.getElementById('card');
+  const toBack = document.getElementById('toBack');
+  const toFront = document.getElementById('toFront');
+  const flipSound = document.getElementById('flipSound');
 
-    toBack.addEventListener('click', () => {
-      card.classList.add('flipped');
-      flipSound.play();
-    });
+  const text = `A person with a Bachelor of science Degree in Electromechanical Engineering and Certified in Different Courses and Activity. Experienced in Engineering jobs, coding and Marketing Both Professionally and non-Professionally. Fluent with English and Amharic`;
+  let i = 0;
 
-    toFront.addEventListener('click', () => {
-      card.classList.remove('flipped');
-      flipSound.play();
-    });
+  function typeWriter() {
+    const element = document.getElementById("typewriter");
+    element.innerHTML = "";
+    i = 0;
+    const interval = setInterval(() => {
+      element.innerHTML += text.charAt(i);
+      i++;
+      if (i >= text.length) clearInterval(interval);
+    }, 30);
+  }
+
+  toBack.addEventListener('click', () => {
+    card.classList.add('flipped');
+    flipSound.play();
+    // confetti({
+    //   particleCount: 100,
+    //   spread: 70,
+    //   origin: { y: 0.6 }
+    // });
+    typeWriter();
+  });
+
+  toFront.addEventListener('click', () => {
+    card.classList.remove('flipped');
+    flipSound.play();
+  });
